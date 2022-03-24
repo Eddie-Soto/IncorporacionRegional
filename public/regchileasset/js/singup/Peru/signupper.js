56b6243df6f7a1a0f54f4c0c51f9fe42548acc05
Eddie-Soto <esoto@nikkenlatam.com>
2022-03-23 19:36:49 -0600

    Update signupper.js

--- public/regchileasset/js/singup/Peru/signupper.js
+++ public/regchileasset/js/singup/Peru/signupper.js
@@ -1,40 +1,4 @@
-//var URLactual = window.location;
-
-
-/**
-* Función que Obtiene las tipos de docuemnto de Perú
-*/
-function getDocuments(){
-  var type_person = $('#type_per').val();
-  var country = $('#country').val();
-       // var regi = regis.replace("'", "apost");
-        //string.replace(searchvalue, newvalue)
-        $.ajax({
-          type: "GET",
-          url: '/typedocuments',
-          dataType: "json",
-          contentType: "text/json; charset=UTF-8",
-          data: {
-            type_person: type_person,
-            country: country
-          },
-          success: function(data){
-            $("#typedocument").find('option').remove();
-            $("#typedocument").append('<option value="" selected>'+selreg+'</option>');
-         //   $("#region").append('<option value="" selected>selecciona una opcion</option>');
-           // $("#comuna").append('<option value="" selected>selecciona una opcion</option>');
-          //  $("#ciudad").append('<option value="" selected>selecciona una opcion</option>');
-          $.each(data,function(key, registro) {
-
-            $("#comuna").append('<option value='+registro.id_type.replace(/ /g, "%")+'>'+registro.name+'</option>');
-          });
-        },
-        error: function(data) {
-
-        }
-      });
-      }
-
-
- getDocuments();
-      
+  $(function() 
+  {
+    console.log( "Ha ocurrido document.ready: documento listo" );
+  });
