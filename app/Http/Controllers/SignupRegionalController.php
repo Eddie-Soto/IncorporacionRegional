6a7fdf4d4b01f6ac21476dfe5c149f053c473557
Eddie-Soto <esoto@nikkenlatam.com>
2022-03-28 14:50:58 -0600

    Update SignupRegionalController.php

--- app/Http/Controllers/SignupRegionalController.php
+++ app/Http/Controllers/SignupRegionalController.php
@@ -464,7 +464,8 @@ public function storePeru(Request $request){
     $titular_name_empresa = $request->input('namelegarrepresentative').trim("");
     $titular_name_empresa = strtoupper($titular_name_empresa);
     if ($type_per == 0) {
-    	echo "es empresa";
+    	echo "es empresa".$titular_name_empresa;
+
     	exit;
     }
 
